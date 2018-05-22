a8fd99297548116ae9b81dd543ccce3ee00e24dc
santoshbt <bt.santosh01@gmail.com>
2018-05-22 17:03:58 +0530

    testing for get todos by id

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get('/todos/:id', (req,  res) => {
       if(!todo){
         return res.status(404).send();
       }
-      return res.status(200).send(JSON.stringify(todo, undefined, 2));
+      return res.send({todo});
     }, (e) => {
       return res.status(400).send();
     });
