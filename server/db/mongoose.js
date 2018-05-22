6f2ffa9b0202179d420157e929be3b4c99139e22
santoshbt <bt.santosh01@gmail.com>
2018-05-22 17:57:56 +0530

    db connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoAp',
   mlab: 'mongodb://test_user:test123@ds139761.mlab.com:39761/todoapp'
 }
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect(db.mlab || db.localhost);
 
 module.exports = { mongoose }
