ffbc8a57c97684d3db5bbfef7dbe6428384def8d
santoshbt <bt.santosh01@gmail.com>
2018-05-23 20:20:22 +0530

    added test to delete todo

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.delete('/todos/:id', (req, res) => {
       }
       res.send({todo});
     }).catch((e) => {
-      res.status(404).send();
+      res.status(400).send();
     });
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -111,8 +111,8 @@ describe('DELETE /todos/:id', () => {
     request(app)
       .delete(`/todos/${hexId}`)
       .expect(200)
-      .expect(() => {
-        expect(res.body.todo.id).toBe(hexId);
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
       }).end((err, res) => {
         if(err) {
           return done(err)
@@ -124,11 +124,18 @@ describe('DELETE /todos/:id', () => {
       });
   });
   //
-  // it('should return 404 if todo not found', (done) => {
-  //
-  // });
-  //
-  // it('should reuturn 404 if object id is invalid', (done) => {
-  //
-  // });
+  it('should return 404 if todo not found', (done) => {
+     var hexId = new ObjectID().toHexString();
+     request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should reuturn 404 if object id is invalid', (done) => {
+    request(app)
+     .delete(`/todos/123abc`)
+     .expect(404)
+     .end(done);
+  });
 })
