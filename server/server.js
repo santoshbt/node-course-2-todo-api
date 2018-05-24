c693260b84571a46d4761f722de559e4f875d122
santoshbt <bt.santosh01@gmail.com>
2018-05-24 17:29:52 +0530

    added authtoken generation

--- server/server.js
+++ server/server.js
@@ -104,9 +104,13 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body)
-  user.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    console.log(token);
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
     res.status(400).send(e);
   });
 });
