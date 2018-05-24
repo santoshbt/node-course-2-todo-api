938f051c4ede3628bcc78685521cc52626533551
santoshbt <bt.santosh01@gmail.com>
2018-05-24 19:19:10 +0530

    add GET users/:me and authenticate users

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var obj = new ObjectID();
 
@@ -108,13 +109,15 @@ app.post('/users', (req, res) => {
   user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
-    console.log(token);
     res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   });
 });
 
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
 
 app.listen(3000, () => {
   console.log(`Started on port ${port}`);
