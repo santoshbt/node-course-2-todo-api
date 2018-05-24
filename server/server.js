b33ac0f0f62e46c4d2a4999bc337be62b8353a2f
santoshbt <bt.santosh01@gmail.com>
2018-05-24 12:57:59 +0530

    creating user custom validation

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var obj = new ObjectID();
 
@@ -98,6 +99,18 @@ app.patch('/todos/:id', (req, res) => {
     res.status(400).send();
   })
 });
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body)
+  user.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 
 app.listen(3000, () => {
   console.log(`Started on port ${port}`);
